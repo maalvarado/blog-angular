b1346db075fa8938e3c4b54328cc524efd897e04
Mike Alvarado <go.miguelhdz@gmail.com>
2018-09-02 21:38:29 -0500

    ADD: category AF grid

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'blog-category',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  items$: Observable<any[]>;
+  id: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id');
+      this.items$  = this.db.list(`categories/${this.id}/items`, ref => ref.orderByChild('time').limitToLast( 12 ) ).valueChanges();
+
+      this.items$.subscribe( s => {
+        if(s.length === 0){
+          this.router.navigateByUrl('/404');
+        }
+      });
+    });
   }
 
 }
